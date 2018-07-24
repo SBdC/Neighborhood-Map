a93cfa15445a611b7e4592d077e8f3d97117ac1c
Sara Bento de Castro <denominatu@hotmail.com>
2018-07-24 11:01:47 +0200

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => (
+
+  <div>
+    <header className="App-header">
+      <h1 className="App-title">Fun hangouts in Berlin Mitte</h1>
+    </header>
+  </div>
+
+)
+
+export default Header
