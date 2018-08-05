dbfadaf124bf667bda29289789160d4c77c92f14
Sara Bento de Castro <denominatu@hotmail.com>
2018-08-05 12:37:58 +0200

    added more styles

--- src/InfoWindow.js
+++ src/InfoWindow.js
@@ -9,12 +9,19 @@ class Info extends Component {
     const { currentLocation, photos } = this.props;
     return currentLocation ? (
       <div className="location-info">
-        <button   onClick={() => this.props.closeInfo()}> <div className="icon-close"> <FontAwesomeIcon icon={ faTimes }  /></div></button>
-        <h3>{currentLocation.name}</h3>
-        <h4>{currentLocation.id}</h4>
-          <h5>{currentLocation.title}</h5>
+        <button  onClick={() => this.props.closeInfo()}> <div className="icon-close"> <FontAwesomeIcon icon={ faTimes }  /></div></button>
+        <h2 className = "info-heading">{currentLocation.name}</h2>
+        <p className = "info-p">{currentLocation.title}</p>
+        <div className = "pic-add-wrapper">
+            <div className = "info-img"><img  className="flickerPhoto" alt={currentLocation.name} src={photos}></img></div>
+        <div className = "info-add">
+            <p  className = "info-add-p">{currentLocation.street}</p>
+              <p  className = "info-add-p">{currentLocation.zip}</p>
 
-          <div ><img  className="flickerPhoto" alt={currentLocation.name} src={photos}></img></div>
+        </div>
+
+
+        </div>
       </div>
 
     ) : (
--- src/LocationList.js
+++ src/LocationList.js
@@ -1,48 +1,49 @@
 import React from "react";
 import Search from "./Search.js";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { faFrown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faFrown } from '@fortawesome/free-solid-svg-icons'
 
-const LocationList = props => {
+
+const LocationList = (props) => {
   if (props.filteredLocations.length === 0) {
-    return (
+    return(
       <div style={props.style}>
         <Search {...props} />
         <div className="App-location-list">
           <p> No results found</p>
-          <div className="icon-frown">
-            {" "}
-            <FontAwesomeIcon className="arrow" icon={faFrown} />
-          </div>
+          <div className="icon-frown"> <FontAwesomeIcon className="arrow" icon ={ faFrown }  /></div>
         </div>
-      </div>
+    </div>
     );
+
   }
 
-  return (
+
+  return(
     <div style={props.style}>
       <Search {...props} />
       <div className="App-location-list">
         <ul className="location-list">
+
           {props.filteredLocations.map(location => (
-            <div className="list" key={location.id}>
-              <div className="icon-arrow">
-                {" "}
-                <FontAwesomeIcon className="arrow" icon={faArrowRight} />
-              </div>
-              <li
-                name={location.name}
-                onClick={() => props.onClick({ ...location })}
-              >
-                {location.name}
-              </li>
+            <div className="list"
+              key={location.id}>
+             <div className="icon-arrow"> <FontAwesomeIcon className="arrow" icon ={ faArrowRight }  /></div>
+            <li
+
+              name={location.name}
+              onClick={() => props.onClick({ ...location })}
+            >
+              {location.name}
+            </li>
             </div>
           ))}
         </ul>
       </div>
-    </div>
-  );
-};
+  </div>
+  )
+
+}
 
 export default LocationList;
